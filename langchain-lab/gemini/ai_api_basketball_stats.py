cf96b11c5ecce47f60484eba9706dddcc1018098
elk1o <elk1o.dev@gmail.com>
2026-08-19 20:46:46 +0200

    Added new API enriched langchain AI call

---
+++ langchain-lab/gemini/ai_api_basketball_stats.py
@@ -0,0 +1,62 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+AISTUDIO_APIKEY = os.getenv('AISTUDIO_APIKEY')
+BDL_APIKEY = os.getenv('BALL_DONT_LIE_APIKEY')
+PLAYER = "Jokic"
+
+import requests
+from datetime import date
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+BASE_URL = "https://api.balldontlie.io"
+
+# Set up headers with authentication
+headers = {"Authorization": BDL_APIKEY}
+
+today = "2026-08-16"
+
+# Fetch today's NBA games
+response = requests.get(
+    f"{BASE_URL}/v1/players",
+    headers=headers,
+    params={"search": PLAYER}
+)
+
+# Print the results
+bdl_player_raw_data = response.json()
+
+if bdl_player_raw_data['data']:
+    #print(f"Found player {PLAYER}: {bdl_player_raw_data['data'][0]}")
+    player_data = bdl_player_raw_data['data'][0]
+
+    # Call the Google Generative AI model with prompttemplate with enriched API data
+
+    template = """
+        Eres un experto en estadísticas de baloncesto y análisis de rendimiento de jugadores. A continuación, se te proporcionan los datos de un jugador específico. Tu tarea es entender los datos que se te aportan a continuación y generar un resumen conciso y detallado del jugador, incluyendo cualquier información relevante que pueda ser útil para dar a conocer al jugador. Asegúrate de que el resumen sea claro, informativo y fácil de entender:
+        {player_data}
+    """
+
+    prompt_template = PromptTemplate(
+        input_variables=["player_data"],
+        template=template
+    )
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-3.6-flash",
+        temperature=0,
+        google_api_key=AISTUDIO_APIKEY
+    )
+
+    chain = prompt_template | llm
+
+    print("*****************")
+    print(f"Consulting the basketball expert analyst about the given player's data '{PLAYER}'")
+    print("*****************")
+    print("")
+    print(chain.invoke({"player_data": player_data}).content)
+
+else:
+    print(f'Not found player "{PLAYER}".')
